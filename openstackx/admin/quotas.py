71c68b48b01eed1b479d0aa3b3c2b77217a8d8b0
Jake Dahn <admin@jakedahn.com>
2011-07-07 01:06:49 -0700

    client now calls projects by the name of tenants

--- openstackx/admin/quotas.py
+++ openstackx/admin/quotas.py
@@ -9,7 +9,7 @@ class Quota(base.Resource):
         self.manager.delete(self)
 
     def update(self, *args, **kwargs):
-        self.manager.update(self.project_id, *args, **kwargs)
+        self.manager.update(self.tenant_id, *args, **kwargs)
 
 
 class QuotaManager(base.ManagerWithFind):
@@ -18,24 +18,18 @@ class QuotaManager(base.ManagerWithFind):
     def list(self):
         return self._list("/admin/quotas", "quotas")
 
-    def get(self, project_id):
-        return self._get("/admin/quotas/%s" % (project_id), "quota")
+    def get(self, tenant_id):
+        return self._get("/admin/quotas/%s" % (tenant_id), "quota")
 
-    def create(self, project_id, resource, limit):
-        body = {"quota": {'project_id': project_id, 'resource': resource,
-                          'hard_limit': limit}}
-
-        return self._create('/admin/quotas', body, "quota")
-
-    def update(self, project_id, **kwargs):
+    def update(self, tenant_id, **kwargs):
         attributes = {}
         for arg in kwargs:
             attributes[arg] = kwargs[arg]
 
         body = {"quota": attributes}
 
-        return self._update('/admin/quotas/%s' % (project_id), body)
+        return self._update('/admin/quotas/%s' % (tenant_id), body)
 
-    def delete(self, project_id):
-        self._delete("/admin/quotas/%s" % (project_id))
+    def delete(self, tenant_id):
+        self._delete("/admin/quotas/%s" % (tenant_id))
 
