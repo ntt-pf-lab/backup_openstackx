171636677c19ac5f81cf05a1716e21add28f5646
Carlo Truijllo <truijllo@crs4.it>
2011-08-11 16:59:48 +0200

    kludge to have updated version of extensions working with different version of nova

--- extensions/admin.py
+++ extensions/admin.py
@@ -303,7 +303,11 @@ class PrivilegedServerController(openstack_api.servers.ControllerV11):
         # the implementation of the Servers API, which is in flux
 
         try:
-            servers = self._items(req, is_detail=True)
+            try:
+            	# KLUDGE to make this extension working with different nova branches
+	        # in a soon future there will be only:
+	        # servers = self._get_servers(req, is_detail=True)
+                servers = getattr(self,'_get_servers',self._items)(req, is_detail=True)
         except exception.Invalid as err:
             return exc.HTTPBadRequest(explanation=str(err))
 
